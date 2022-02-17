f854335219f12dba89260f5e1b59426a05add266
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2022-02-17 20:15:03 +0800

    refactor: update isLink and trim extracted links

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,9 +1,9 @@
-export const URL_MATCH = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/g;
+const URL_MATCH = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/g;
 
 export function isLink(string: string): boolean {
-  return URL_MATCH.test(string);
+  return !!string.match(URL_MATCH)?.length;
 }
 
 export function extractLinks(string: string): string[] {
-  return string.match(URL_MATCH) as string[];
+  return string.match(URL_MATCH)?.map((link) => link.trim()) ?? [];
 }
